ea9c4dfaccff2a075c9f23441337f84859d5dc2b
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-10 20:30:33 +0530

    commited

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "aws" {
   
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraformbucket-22"
-    key = "terraform.tfstate"
-    region = "ap-south-1"  
-    dynamodb_table = "mytable"	  
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraformbucket-22"
+#     key = "terraform.tfstate"
+#     region = "ap-south-1"  
+#     dynamodb_table = "mytable"	  
+#   }
+# }
 
 resource "aws_vpc" "myterraformvpc" {
     cidr_block = var.cidr_block
